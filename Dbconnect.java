047d253a9efdd11c4afa637dd1d752bda2a51c7f
challamtejareddy <tejareddychallam44.gmail.com>
2018-09-04 20:43:30 +0530

    updated commit stmt

--- Dbconnect.java
+++ Dbconnect.java
@@ -3,3 +3,4 @@ this is devops training  purpose
 this java based application performace tool 
 and this is used to source code management purpose
  this is one moretime update
+ this is a second type commit stmt
