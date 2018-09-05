18361cdbfc1e13ea0b0a240d1c35c56de4dfdc51
tejareddychallam <tejareddychallam44@gmial.com>
2018-09-05 16:23:16 +0530

    upadted to testing branch

--- Dbconnect.java
+++ Dbconnect.java
@@ -1,5 +1,4 @@
 hello frds
 this is devops training  purpose 
-this java based application performace tool 
-and this is used to source code management purpose
+this java based and this is used to source code management purpose
  this is one moretime update
